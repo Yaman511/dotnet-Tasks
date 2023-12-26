2f8bb75283a814f895f169857c738cdc37617c2a
Yaman511 <yamanb511@gmail.com>
2023-12-26 11:44:56 +0300

    Task 3 requirements finished

---
+++ model/FileContent.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2.model
+{
+    public class FileContent
+    {
+        public IFormFile? File { get; set; }
+        public string FileName { get; set; }
+        public string Owner { get; set; }
+        public string? Description { get; set; }
+        public QueryType QueryType { get; set; }
+        public string CreationDate { get; set; }
+        public string ModificationDate { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
+        public SortType SortType { get; set; }
+        public string[] Name { get; set; }
+    }
+}
---
+++ model/FilterByUserFormData.cs
@@ -0,0 +1,13 @@
+﻿    namespace Task2.model
+    {
+
+    
+        public class FilterByUserFormData
+        {
+            public string[] Name { get; set; }
+            public string StartDate { get; set; }
+            public string EndDate { get; set; }
+            public SortType SortType { get; set;}
+
+        }
+    }
